a5669b4e6ad6d28317a4f86473dddda5227b1468
wwwwwww <mik373737373737@gmail.com>
2020-11-20 18:07:15 +0200

    v1

---
+++ DemoPlugin1/MyFirstPlugin.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DemoPlugin1
+{
+   public class MyFirstPlugin: IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.MessageName != "Create") { return; }
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if (entity.LogicalName != "account") { return; }
+
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    if (context.Stage != 40)
+                    {
+                        Entity updateAccount = new Entity("account");
+                        updateAccount.Id = entity.Id;
+                        updateAccount.Attributes["description"] = "Hello World";
+                        service.Update(updateAccount);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ DemoPlugin1/MyPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace DemoPlugin1
+{
+    class MyPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if(context.MessageName != "Create") { return; }
+
+            if(context.InputParameters.Contains("Target")&&context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                if(entity.LogicalName != "account") { return; }
+
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+////
+                }
+                catch(Exception ex)
+                {
+                    tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
